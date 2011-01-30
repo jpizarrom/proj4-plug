178c3b813340a43a8889c8dc845699a1aafa161e
Juan Pizarro <jpizarro@doingit.cl>
2011-01-30 02:00:53 -0300

    add plug skel

---
+++ src/org/openstreetmap/josm/plugins/proj4/PROJ4UploadHook.java
@@ -0,0 +1,25 @@
+package org.openstreetmap.josm.plugins.proj4;
+
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.actions.upload.UploadHook;
+import org.openstreetmap.josm.data.APIDataSet;
+
+public class PROJ4UploadHook implements UploadHook {
+
+    private final Proj4 plugin;
+
+    public PROJ4UploadHook(Proj4 plugin)
+    {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean checkUpload(APIDataSet arg0) {
+        if (Main.proj.toCode().equals(ProjectionPROJ4.getProjCode())) {
+            plugin.toggleProjection();
+        }
+        return true;
+    }
+
+}
---
+++ src/org/openstreetmap/josm/plugins/proj4/ProjectionPROJ4.java
@@ -0,0 +1,22 @@
+package org.openstreetmap.josm.plugins.proj4;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
+import java.awt.geom.Point2D;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.openstreetmap.josm.data.Bounds;
+import org.openstreetmap.josm.data.coor.EastNorth;
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.tools.GBC;
+
+public class ProjectionPROJ4 implements org.openstreetmap.josm.data.projection.Projection, org.openstreetmap.josm.data.projection.ProjectionSubPrefs {
+
+}
